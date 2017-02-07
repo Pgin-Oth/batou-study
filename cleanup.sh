4d090b272026066b7e41030d5714807ca833eb5b
Pgin Oth <pgin.oth@gmail.com>
2017-02-07 10:37:27 +0100

    Add .gitkeep again.

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 cd /home/vagrant/deployment/work/batou/checkout/
-ls -A1 | xargs rm -rdf
+rm -rdf batou
 cd /home/vagrant/deployment/work/zope/checkout/
-ls -A1 | xargs rm -rdf
+rm -rdf zope
 cd /home/vagrant/deployment/work/roundcube/checkout/
-ls -A1 | xargs rm -rdf
+rm -rdf roundcube
 cd /home/vagrant/deployment/work/test/checkout/
-ls -A1 | xargs rm -rdf
+rm -rdf test
 cd /home/vagrant/deployment/work/test2/checkout/
-ls -A1 | xargs rm -rdf
+rm -rdf test2
 cd /home/vagrant/deployment/work/test3/checkout/
-ls -A1 | xargs rm -rdf
+rm -rdf test3
