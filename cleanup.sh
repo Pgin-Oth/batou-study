79cbbd1a9ff3dc5e457b67791f83d6a570609337
Pgin Oth <pgin.oth@gmail.com>
2017-02-09 10:05:27 +0100

    New cleanup logic.

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,5 @@ prj_path=`pwd`
 for line in $(cat tmp_share_folder.cfg)
 do
     local=`echo ${line} | cut -d "," -f1`
-    cd ${prj_path}/${local}
-    ls -A | grep -v .gitkeep | xargs rm -rdf
+    cd "${prj_path}/${local}" && rm -rdf * && touch .gitkeep
 done
